66885ddba16c5111628265c5edf688eb8a7d4760
Yiew Jun Wei <2601714@sit.singaporetech.edu.sg>
2026-09-11 16:47:30 +0800

    Keep a running total of the inventory

--- auditor.py
+++ auditor.py
@@ -7,7 +7,7 @@ stock_quantity = input("Enter stock quantity: ")
 # 2. Run in a continuous loop asking user to enter a stock quantity, until the user types quit. 
 while stock_quantity != "quit":
 
-    # 4. and 5. Check whether the input contain positive whole number digits
+    # 4. and 5. Check whether the input contains positive whole numbers.
     if not stock_quantity.isdigit():
         print("Error: Invalid input. Please enter a positive valid number.")
         failed_entries = failed_entries + 1
@@ -15,8 +15,8 @@ while stock_quantity != "quit":
     else:
         # 3. Accept stock values as integers
         stock_quantity = int(stock_quantity)
+        # 6. Keep a running total of the inventory
         inventory = inventory + stock_quantity
-        print("Current inventory: " + str(inventory))
-
+        print ("Inventory updated. Current inventory: " + str(inventory))
 
     stock_quantity = input("Enter new stock quantity (or 'quit' to exit): ")
