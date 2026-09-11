ab88f4ec619994f76ad5676bbe7a9ccff6ecf7e0
Yiew Jun Wei <2601714@sit.singaporetech.edu.sg>
2026-09-11 16:41:12 +0800

    Accept stock values as integers. Check whether the input contains positive whole numbers.

--- auditor.py
+++ auditor.py
@@ -1,11 +1,22 @@
 # 1. Initialize inventory to zero
 inventory = 0
+failed_entries = 0
 
 stock_quantity = input("Enter stock quantity: ")
 
 # 2. Run in a continuous loop asking user to enter a stock quantity, until the user types quit. 
 while stock_quantity != "quit":
-    inventory = inventory + int(stock_quantity)
-    print("Current inventory: " + str(inventory))
+
+    # 4. and 5. Check whether the input contain positive whole number digits
+    if not stock_quantity.isdigit():
+        print("Error: Invalid input. Please enter a positive valid number.")
+        failed_entries = failed_entries + 1
+
+    else:
+        # 3. Accept stock values as integers
+        stock_quantity = int(stock_quantity)
+        inventory = inventory + stock_quantity
+        print("Current inventory: " + str(inventory))
+
 
     stock_quantity = input("Enter new stock quantity (or 'quit' to exit): ")
