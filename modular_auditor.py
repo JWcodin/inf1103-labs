6719c0dc0028214c88bfac662a835fd0fb51ac6d
Yiew Jun Wei <2601714@sit.singaporetech.edu.sg>
2026-09-18 21:01:58 +0800

    get_valid_input(): Handles the prompt, handles input validation, and returns a valid integer or a quit signal.

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,30 +1,32 @@
-# 1. Initialize inventory to zero
+# 1. get_valid_input(): Handles the prompt, handles input validation, and returns a valid integer or a "quit" signal. 
+def get_valid_input():
+    stock_quantity = input("Enter new stock quantity (or 'quit' to exit): ")
+
+    if stock_quantity == "quit":
+        return "quit"
+
+    if not stock_quantity.isdigit():
+        print("Error: Invalid input. Please enter a positive valid number.")
+        return None
+    else:
+        return int(stock_quantity)
+
+# Initialize inventory to zero
 inventory = 0
 failed_entries = 0
 
-stock_quantity = input("Enter stock quantity: ")
+#Run in a continuous loop asking user to enter a stock quantity, until the user types quit. 
+while True:
 
-# 2. Run in a continuous loop asking user to enter a stock quantity, until the user types quit. 
-while stock_quantity != "quit":
+   stock_quantity = get_valid_input()
+   
+   if stock_quantity == "quit":
+        break
 
-    # 4. and 5. Check whether the input contains positive whole numbers.
-    if not stock_quantity.isdigit():
-        print("Error: Invalid input. Please enter a positive valid number.")
+   if stock_quantity is None:
         failed_entries = failed_entries + 1
+        continue
 
-    else:
-        # 3. Accept stock values as integers
-        stock_quantity = int(stock_quantity)
-        # 6. Keep a running total of the inventory
-        inventory = inventory + stock_quantity
-        print ("Inventory updated. Current inventory: " + str(inventory))
-
-        # 7. If the total inventory exceeds 500 units, print an alert and break the loop immediately. 
-        if inventory > 500:
-            print("Alert: Inventory is full.")
-            break
-
-    stock_quantity = input("Enter new stock quantity (or 'quit' to exit): ")
 
 # 8. Reporting: When the user types quit, print the Total Units Processed and the Number of Failed/Rejected Entries.
 print("\n--- Inventory Report ---")
